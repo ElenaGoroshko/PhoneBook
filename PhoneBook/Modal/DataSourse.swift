b1bb2b839e5f9f4ac3d05288e965583c4e42d8b6
ElenaGoroshko <elena.goroshko@gmail.com>
2017-11-17 16:15:02 +0200

    no message

---
+++ PhoneBook/Modal/DataSourse.swift
@@ -0,0 +1,9 @@
+//
+//  DataSourse.swift
+//  PhoneBook
+//
+//  Created by Elena Goroshko on 11/17/17.
+//  Copyright © 2017 Elena Goroshko. All rights reserved.
+//
+
+import Foundation
---
+++ PhoneBook/Modal/Person.swift
@@ -0,0 +1,16 @@
+//
+//  Person.swift
+//  PhoneBook
+//
+//  Created by Elena Goroshko on 11/17/17.
+//  Copyright © 2017 Elena Goroshko. All rights reserved.
+//
+
+import UIKit
+
+struct Person {
+    var firstName: String
+    var lastName: String
+    var pfoneNumber: Int
+    var pfoto: UIImage
+}
